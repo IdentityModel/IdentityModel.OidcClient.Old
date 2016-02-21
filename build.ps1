c88c1f4db9503cf5cef2ba135a4cc57363fc3d7e
Dominick Baier <dbaier@leastprivilege.com>
2016-02-21 14:31:03 +0100

    add build script

---
+++ build.ps1
@@ -0,0 +1,33 @@
+Param(
+	[string]$buildNumber = "0",
+	[string]$preRelease = $null
+)
+
+$exists = Test-Path nuget.exe
+
+if ($exists -eq $false) {
+    $source = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+    Invoke-WebRequest $source -OutFile nuget.exe
+}
+
+.\nuget.exe install psake -ExcludeVersion -o packages -nocache
+
+gci .\source -Recurse "packages.config" |% {
+	"Restoring " + $_.FullName
+	.\nuget.exe install $_.FullName -o .\source\packages
+}
+
+Import-Module .\packages\psake\tools\psake.psm1
+
+if(Test-Path Env:\APPVEYOR_BUILD_NUMBER){
+	$buildNumber = [int]$Env:APPVEYOR_BUILD_NUMBER
+	Write-Host "Using APPVEYOR_BUILD_NUMBER"
+
+	$task = "appVeyor"
+}
+
+"Build number $buildNumber"
+
+Invoke-Psake .\default.ps1 $task -framework "4.0x64" -properties @{ buildNumber=$buildNumber; preRelease=$preRelease }
+
+Remove-Module psake
---
+++ default.ps1
@@ -0,0 +1,73 @@
+properties {
+	$base_directory = Resolve-Path .
+	$src_directory = "$base_directory\src\IdentityModel.OidcClient"
+	$output_directory = "$base_directory\build"
+	$dist_directory = "$base_directory\distribution"
+	$sln_file = "$base_directory\IdentityModel.OidcClient.sln"
+	$target_config = "Release"
+	$nuget_path = "$base_directory\nuget.exe"
+
+	$buildNumber = 0;
+	$version = "1.8.0.0"
+	$preRelease = $null
+}
+
+task default -depends Clean, CreateNuGetPackage
+task appVeyor -depends Clean, CreateNuGetPackage
+
+task Clean {
+	rmdir $output_directory -ea SilentlyContinue -recurse
+	rmdir $dist_directory -ea SilentlyContinue -recurse
+	exec { msbuild /nologo /verbosity:quiet $sln_file /p:Configuration=$target_config /t:Clean }
+}
+
+task Compile -depends UpdateVersion {
+	exec { & "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" /nologo /verbosity:q $sln_file /p:Configuration=$target_config /p:TargetFrameworkVersion=v4.5 }
+}
+
+task UpdateVersion {
+	$vSplit = $version.Split('.')
+	if($vSplit.Length -ne 4)
+	{
+		throw "Version number is invalid. Must be in the form of 0.0.0.0"
+	}
+	$major = $vSplit[0]
+	$minor = $vSplit[1]
+	$patch = $vSplit[2]
+	$assemblyFileVersion =  "$major.$minor.$patch.$buildNumber"
+	$assemblyVersion = "$major.$minor.0.0"
+	$versionAssemblyInfoFile = "$src_directory/VersionAssemblyInfo.cs"
+	"using System.Reflection;" > $versionAssemblyInfoFile
+	"" >> $versionAssemblyInfoFile
+	"[assembly: AssemblyVersion(""$assemblyVersion"")]" >> $versionAssemblyInfoFile
+	"[assembly: AssemblyFileVersion(""$assemblyFileVersion"")]" >> $versionAssemblyInfoFile
+}
+
+task CreateNuGetPackage -depends Compile {
+	$vSplit = $version.Split('.')
+	if($vSplit.Length -ne 4)
+	{
+		throw "Version number is invalid. Must be in the form of 0.0.0.0"
+	}
+	$major = $vSplit[0]
+	$minor = $vSplit[1]
+	$patch = $vSplit[2]
+	$packageVersion =  "$major.$minor.$patch"
+	if($preRelease){
+		$packageVersion = "$packageVersion-$preRelease"
+	}
+
+	if ($buildNumber -ne 0){
+		$packageVersion = $packageVersion + "-build" + $buildNumber.ToString().PadLeft(5,'0')
+	}
+
+	new-item $dist_directory -type directory
+
+	copy-item $base_directory\IdentityModel.OidcClient.nuspec $dist_directory
+
+	new-item $dist_directory\lib\portable-net45+wp80+win8+wpa81 -type directory
+	copy-item $output_directory\IdentityModel.OidcClient.dll $dist_directory\lib\portable-net45+wp80+win8+wpa81\
+	copy-item $output_directory\IdentityModel.OidcClient.pdb $dist_directory\lib\portable-net45+wp80+win8+wpa81\
+
+	exec { . $nuget_path pack $dist_directory\IdentityModel.OidcClient.nuspec -BasePath $dist_directory -o $dist_directory -version $packageVersion }
+}
