f1af02e1ca2217c2360c8be877da4819349c14f1
Dominick Baier <dbaier@leastprivilege.com>
2016-07-17 17:35:14 +0200

    added logging for default id token validator

--- src/Shared/IdentityTokenValidation/DefaultIdentityTokenValidator.cs
+++ src/Shared/IdentityTokenValidation/DefaultIdentityTokenValidator.cs
@@ -7,15 +7,21 @@ using System.Threading.Tasks;
 using System.Linq;
 using JosePCL.Keys.Rsa;
 using Newtonsoft.Json.Linq;
+using IdentityModel.OidcClient.Logging;
 
 namespace IdentityModel.OidcClient.IdentityTokenValidation
 {
     public class DefaultIdentityTokenValidator : IIdentityTokenValidator
     {
+        private static readonly ILog Logger = LogProvider.For<DefaultIdentityTokenValidator>();
+
         public TimeSpan ClockSkew { get; set; } = TimeSpan.FromMinutes(5);
 
         public Task<IdentityTokenValidationResult> ValidateAsync(string identityToken, string clientId, ProviderInformation providerInformation)
         {
+            Logger.Debug("starting identity token validation");
+            Logger.Debug("identity token: " + identityToken);
+
             var fail = new IdentityTokenValidationResult
             {
                 Success = false
@@ -26,34 +32,48 @@ namespace IdentityModel.OidcClient.IdentityTokenValidation
             var pubKey = PublicKey.New(e, n);
 
             var json = JosePCL.Jwt.Decode(identityToken, pubKey);
+            Logger.Debug("decoded JWT: " + json);
+
             var payload = JObject.Parse(json);
 
             var issuer = payload["iss"].ToString();
+            Logger.Debug("issuer: " + issuer);
+
             var audience = payload["aud"].ToString();
+            Logger.Debug("audience: " + issuer);
 
             if (issuer != providerInformation.IssuerName)
             {
                 fail.Error = "Invalid issuer name";
+                Logger.Error(fail.Error);
+
                 return Task.FromResult(fail);
             }
 
             if (audience != clientId)
             {
                 fail.Error = "Invalid audience";
+                Logger.Error(fail.Error);
+
                 return Task.FromResult(fail);
             }
 
+            var utcNow = DateTime.UtcNow;
             var exp = payload.Value<long>("exp");
             var nbf = payload.Value<long?>("nbf");
 
-            var utcNow = DateTime.UtcNow;
-
+            Logger.Debug("exp: " + exp.ToString());
+            
             if (nbf != null)
             {
+                Logger.Debug("nbf: " + nbf.ToString());
+
                 var notBefore = nbf.Value.ToDateTimeFromEpoch();
                 if (notBefore > utcNow.Add(ClockSkew))
                 {
                     fail.Error = "Token not valid yet";
+                    Logger.Error(fail.Error);
+
                     return Task.FromResult(fail);
                 }
             }
@@ -62,8 +82,12 @@ namespace IdentityModel.OidcClient.IdentityTokenValidation
             if (expires < utcNow.Add(ClockSkew.Negate()))
             {
                 fail.Error = "Token expired";
+                Logger.Error(fail.Error);
+
                 return Task.FromResult(fail);
             }
+
+            Logger.Info("identity token validation success");
 
             return Task.FromResult(new IdentityTokenValidationResult
             {
